4a31decd9c03aee66ad37a3d69e45c50858b13da
Kevin Tung <kekekevin@gmail.com>
2015-08-09 17:33:15 -0500

    drag and drop

--- app/components/lane-task.js
+++ app/components/lane-task.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'a',
-  classNames: ['list-group-item'],
-  attributeBindings: ['href'],
-  href: "#"
+  tagName: "a",
+  classNames: ["list-group-item"],
+  attributeBindings: ['href', "draggable"],
+  href: "#",
+  draggable: true,
+
+  dragStart(event) {
+    return event.dataTransfer.setData("text/data", this.get("task"))
+  }
 });
--- app/components/swim-lane.js
+++ app/components/swim-lane.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['col-md-3']
+  classNames: ['col-md-3'],
+
+  dragLeave(event) {
+    event.preventDefault();
+  },
+
+  dragOver(event) {
+    event.preventDefault();
+  },
+
+  drop(event) {
+    event.preventDefault();
+
+    var data = event.dataTransfer.getData("text/data");
+    this.sendAction('dropped', data);
+  }
 });
