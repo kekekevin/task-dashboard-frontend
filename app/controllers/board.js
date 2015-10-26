e595564f4069eef780b385016d3c2f3d9439a38b
Kevin Tung <kekekevin@gmail.com>
2015-10-26 07:39:41 -0500

    save task

--- app/controllers/board.js
+++ app/controllers/board.js
@@ -8,7 +8,7 @@ export default Ember.Controller.extend({
     addTask(taskId, swimLaneId) {
       this.store.find("task", taskId).then((task) => {
         this.store.find("swim-lane", swimLaneId).then((swimLane) => {
-          var tasks = swimLane.get("tasks")
+          var tasks = swimLane.get("tasks");
           tasks.pushObject(task);
           task.save();
         });
