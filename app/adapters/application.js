651dbdb54d83a201b765613468463f64820f33b8
Kevin Tung <kekekevin@gmail.com>
2015-11-08 12:24:21 -0600

    csrf token

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -9,5 +9,10 @@ export default ActiveModelAdapter.extend({
   pathForType: function(modelName) {
     var underscore = Ember.String.underscore(modelName);
     return Ember.String.pluralize(underscore);
-  }
+  },
+  headers: Ember.computed(function() {
+    return {
+      "X-XSRF-TOKEN": decodeURIComponent(Ember.get(document.cookie.match(/XSRF\-TOKEN\=([^;]*)/), "1"))
+    };
+  }).volatile(),
 });
